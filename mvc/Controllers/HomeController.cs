f011da69b5f631f9824e17064dd643ef593d74a8
sinancesur34 <sinancesur75@gmail.com>
2023-07-07 23:18:26 +0300

    mesaj

--- mvc/Controllers/HomeController.cs
+++ mvc/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using sehirler_cascading.Models;
 
-namespace mvc.Controllers
+namespace sehirler_cascading.Controllers
 {
     public class HomeController : Controller
     {
+        public KayipDB db = new KayipDB();
         public ActionResult Index()
         {
             return View();
@@ -26,9 +29,47 @@ namespace mvc.Controllers
 
             return View();
         }
-        public ActionResult Test()
+        //DenemeEntities db = new DenemeEntities();
+        Class1 cs = new Class1();
+        public ActionResult Deneme()
         {
-            return View();
+            cs.Sehirler = new SelectList(db.tbliller, "id", "ad");
+            cs.ilceler = new SelectList(Enumerable.Empty<SelectListItem>());
+            return View(cs);
+        }
+        public ActionResult ilcegetir(int p)
+        {
+            var ilceler = (from x in db.Tblilceler
+                           join y in db.tbliller on x.ilid equals y.id
+                           where x.tbliller.id == p
+                           select new
+                           {
+                               Text = x.ad,
+                               value = x.id
+                           }).ToList();
+
+            var ilceList = db.Tblilceler
+                .Where(x => x.ilid == p)
+                .Select(x => new {
+                    Text = x.ad,
+                    value = x.id
+                }).ToList();
+            ilceList.Insert(0, new
+            {
+                Text = "Lütfen Seçiniz",
+                value = 0
+            });
+
+            return Json(ilceList, JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult Kayipilan()
+        {
+
+
+            cs.Sehirler = new SelectList(db.tbliller, "id", "ad");
+            cs.ilceler = new SelectList(Enumerable.Empty<SelectListItem>());
+            return View(cs);
         }
     }
 }
